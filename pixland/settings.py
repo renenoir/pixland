a8b425dbc6f21870f4cc569eb1b5bf030a1d0dbc
renenoir <ms.unknown.k@gmail.com>
2020-03-19 19:38:18 +0300

    Added AWS settings for heroku

--- pixland/settings.py
+++ pixland/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'storages',
 
     # Сторонние приложения
     'bootstrap4',
@@ -164,6 +165,18 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     # Конфигурация статических ресурсов
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'static'
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = 'pixlandbucket'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
